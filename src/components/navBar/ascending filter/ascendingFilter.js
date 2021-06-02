6eda67251ce1ed3286da592b5dd7a03ce63de6bb
Axelkroos <Axelkroos@mail.ru>
2021-06-02 20:18:09 +0300

    first commit

--- src/components/navBar/ascending filter/ascendingFilter.js
+++ src/components/navBar/ascending filter/ascendingFilter.js
@@ -4,23 +4,23 @@ import classes from './ascendingFilter.module.css'
 class AscendingSort extends React.Component {
     render(props) {
         return <div>
-            <h5>Сортировать</h5>
-            <div className="form_radio">
+            <h3 className={classes.AscendingSort}>Сортировать</h3>
+            <div className="form_radio" className={classes.AscendingSort}>
                 <input id="radio-1" type="radio" name="radio" value="1" onClick={(event => this.props.clickAscendingOrder(event.target.value, this.props.price))}/>
                 <label htmlFor="radio-1">По возрастанию цены</label>
             </div>
 
-            <div className="form_radio">
+            <div className="form_radio" className={classes.AscendingSort}>
                 <input id="radio-2" type="radio" name="radio" value="2" onClick={(event => this.props.clickAscendingOrder(event.target.value))}/>
                 <label htmlFor="radio-2">По убыванию цены</label>
             </div>
 
-            <div className="form_radio">
+            <div className="form_radio" className={classes.AscendingSort}>
                 <input id="radio-3" type="radio" name="radio" value="3" onClick={(event => this.props.clickAscendingOrder(event.target.value))}/>
                 <label htmlFor="radio-3">По времени туда</label>
             </div>
 
-            <div className="form_radio">
+            <div className="form_radio" className={classes.AscendingSort}>
                 <input id="radio-4" type="radio" name="radio" value="4" onClick={(event => this.props.clickAscendingOrder(event.target.value))}/>
                 <label htmlFor="radio-4">По времени обратно</label>
             </div>
--- src/components/navBar/transferFilter/transferFilter.js
+++ src/components/navBar/transferFilter/transferFilter.js
@@ -3,16 +3,18 @@ import classes from './transferFilter.module.css'
 
 class TransferFilter extends React.Component {
     render(props) {
-        return <div>
+        return <div className={classes.AllTransferFilter}>
             <h3>Фильтровать</h3>
             <div>
-                <div>
-                    <input id="radio-5" type="radio" name="radio" value="5" onClick={(event => this.props.clickAscendingOrder(event.target.value))}/>
+                <div className={classes.TransferFilter}>
+                    <input id="radio-5" type="radio" name="radio" value="5"
+                           onClick={(event => this.props.clickAscendingOrder(event.target.value))}/>
                     <label htmlFor="radio-5">1 пересадка</label>
                 </div>
 
-                <div>
-                    <input id="radio-6" type="radio" name="radio" value="6" onClick={(event => this.props.clickAscendingOrder(event.target.value))}/>
+                <div className={classes.TransferFilter}>
+                    <input id="radio-6" type="radio" name="radio" value="6"
+                           onClick={(event => this.props.clickAscendingOrder(event.target.value))}/>
                     <label htmlFor="radio-6">Без пересадок</label>
                 </div>
             </div>
